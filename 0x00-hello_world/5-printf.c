60d9a48cab4d43d7efad5360913a531c48ff792b
toosweets07 <toosweets05@gmail.com>
2023-07-13 09:12:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -A programe that print a line using the printf function
+ * main - A programe that print a line using the printf function
  * Return: 0 (success)
  */
 
