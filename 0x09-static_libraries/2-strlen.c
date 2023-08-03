140ced1b8e3e4a147ebf8445ae355a2a87cd7c37
toosweets07 <toosweets05@gmail.com>
2023-08-03 11:27:25 -0700

    apdates

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,4 +1,4 @@
-clude "main.h"
+#include "main.h"
 /**
  * _strlen - returns the length of a string
  * @s: string
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,11 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
 /**
- * main - Entry point
- *
+ * _puts - Entry point
+ *@s: hello
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	_puts(s);
 }
